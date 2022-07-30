6f511df3d8de307456e9850a6412cabd0700598b
Balaniuk <balaniuk777@gmail.com>
2022-07-30 12:03:40 +0200

    version 0.0

---
+++ main.js
@@ -0,0 +1,26 @@
+const counterNode = document.querySelector('.js-counter');
+const incBtnNode = document.querySelector('.js-inc-btn');
+const incClearNode = document.querySelector('.js-clear-btn');
+
+let counter = 0;
+function increment () {
+    counter += 1;
+};
+
+function clear () {
+    counter = 0;
+};
+
+function render () {
+    counterNode.innerText = counter;
+};
+
+incBtnNode.addEventListener('click', () => { 
+    increment ();
+    render ();
+});
+
+incClearNode.addEventListener('click', () => { 
+    clear ();
+    render ();
+});
